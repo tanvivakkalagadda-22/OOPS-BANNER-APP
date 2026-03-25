24212ab1112c0d0563a9e28765807b0d515ed532
tanvivakkalagadda-22 <tanvivakkalagadda@gmail.com>
2026-03-26 02:27:58 +0530

    UC2: Add switch-based banner menu

--- src/oopsbannerapp.java
+++ src/oopsbannerapp.java
@@ -1,7 +1,14 @@
-public class oopsbannerapp 
-{
-    public static void main(String[]args)
-{
-    System.out.println("OOPS");
-}
-}
+public class oopsbannerapp {
+    public static void main(String[] args) {
+
+        System.out.println("   ***        ***      ******        **** ");
+        System.out.println(" **   **   **     **   **    **    **   ");
+        System.out.println("**     **  **     **   **     **  **     ");
+        System.out.println("**     **  **     **   **    **    **");
+        System.out.println("**     **  **     **   ******        ***  ");
+        System.out.println("**     **  **     **   **               **");
+        System.out.println("**     **  **     **   **                **");
+        System.out.println(" **   **    **   **    **               **");
+        System.out.println("   ***        ***      **           **** ");
+    }
+} 
