a88692ea23ceac83c21b039faeb9b5a29fa558f3
tanvivakkalagadda-22 <tanvivakkalagadda@gmail.com>
2026-03-26 03:08:52 +0530

    UC2: Added

--- src/oopsbannerapp.java
+++ src/oopsbannerapp.java
@@ -1,14 +1,20 @@
 public class oopsbannerapp {
-     public static void main(String[] args) {
-
-        System.out.println(String.join(" ","   ***   ","     ***   ","   ******   ","     **** "));
-        System.out.println(String.join(" "," **   ** ","  **     ** ","  **    ** ","   **   "));
-        System.out.println(String.join(" ","**     **","  **     ** ","  **     **","  **     "));
-        System.out.println(String.join(" ","**     **","  **     ** ","  **    ** ","   **"));
-        System.out.println(String.join(" ","**     **","  **     ** ","  ******   ","      ***  "));
-        System.out.println(String.join(" ","**     **","  **     ** ","  **       ","         **"));
-        System.out.println(String.join(" ","**     **","  **     ** ","  **        ","         **"));
-        System.out.println(String.join(" "," **   ** ","   **   **  ","  **        ","        **"));
-        System.out.println(String.join(" ","   ***   ","     ***    ","  **         ","   **** "));
+    public static void main(String[] args) {
+        String[] arr = new String[9];
+        arr[0] = (String.join(" ","   ***   ","     ***   ","   ******   ","     **** "));
+        arr[1] = (String.join(" "," **   ** ","  **     ** ","  **    ** ","   **   "));
+        arr[2] = (String.join(" ","**     **","  **     ** ","  **     **","  **     "));
+        arr[3] = (String.join(" ","**     **","  **     ** ","  **    ** ","   **"));
+        arr[4] = (String.join(" ","**     **","  **     ** ","  ******   ","      ***  "));
+        arr[5] = (String.join(" ","**     **","  **     ** ","  **       ","         **"));
+        arr[6] = (String.join(" ","**     **","  **     ** ","  **        ","         **"));
+        arr[7] = (String.join(" "," **   ** ","   **   **  ","  **        ","        **"));
+        arr[8] = (String.join(" ","   ***   ","     ***    ","  **         ","   **** "));
+        
+        for(String line : arr){
+            System.out.print(line);
+            System.out.println();
+        }   
     }
 }
+  
