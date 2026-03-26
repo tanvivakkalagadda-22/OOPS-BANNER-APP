dd0ae3f8b297587e4734c201f4dfc21ce71dd0b5
tanvivakkalagadda-22 <tanvivakkalagadda@gmail.com>
2026-03-26 14:08:56 +0530

    UC5 implemented using String.join optimization

--- src/oopsbannerapp.java
+++ src/oopsbannerapp.java
@@ -1,17 +1,18 @@
 public class oopsbannerapp {
-    public static void main(String[] args) {
-        String[] arr = new String[9];
-        arr[0] = (String.join(" ","   ***   ","     ***   ","   ******   ","     **** "));
-        arr[1] = (String.join(" "," **   ** ","  **     ** ","  **    ** ","   **   "));
-        arr[2] = (String.join(" ","**     **","  **     ** ","  **     **","  **     "));
-        arr[3] = (String.join(" ","**     **","  **     ** ","  **    ** ","   **"));
-        arr[4] = (String.join(" ","**     **","  **     ** ","  ******   ","      ***  "));
-        arr[5] = (String.join(" ","**     **","  **     ** ","  **       ","         **"));
-        arr[6] = (String.join(" ","**     **","  **     ** ","  **        ","         **"));
-        arr[7] = (String.join(" "," **   ** ","   **   **  ","  **        ","        **"));
-        arr[8] = (String.join(" ","   ***   ","     ***    ","  **         ","   **** "));
+   public static void main(String[] args) {
+        String[] lines = {
+        (String.join(" ","   ***   ","     ***   ","   ******   ","     **** ")),
+        (String.join(" "," **    ** ","  **   ** ","   **    ** ","   **   ")),
+         (String.join(" ","**     **","  **     ** ","  **     **","  **     ")),
+         (String.join(" ","**     **","  **     ** ","  **    ** ","   **")),
+         (String.join(" ","**     **","  **     ** ","  ******   ","      ***  ")),
+         (String.join(" ","**     **","  **     ** ","  **       ","         **")),
+         (String.join(" ","**     **","  **     ** ","  **        ","         **")),
+         (String.join(" "," **   ** ","   **   **  ","  **        ","        **")),
+         (String.join(" ","   ***   ","     ***    ","  **         ","   **** ")),
+        };
         
-        for(String line : arr){
+        for(String line : lines){
             System.out.print(line);
             System.out.println();
         }   
